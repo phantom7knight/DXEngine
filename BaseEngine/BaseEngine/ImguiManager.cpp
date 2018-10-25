4f64b1bd1247b4570de5835af87bb59e59c6c885
rohit.tolety <rohit.tolety@DIT2908US>
2018-10-25 12:29:41 -0700

    Fixed  changes and the project settings

--- BaseEngine/BaseEngine/ImguiManager.cpp
+++ BaseEngine/BaseEngine/ImguiManager.cpp
@@ -36,38 +36,38 @@ void TestWindow()
 
 void ImguiManager::Init( HWND handle, ID3D11Device* device, ID3D11DeviceContext* deviceContext)
 {
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-	
-	ImGui_ImplWin32_Init(handle);
-	ImGui_ImplDX11_Init(device, deviceContext);
-	
-	// Setup style
-	ImGui::StyleColorsDark();
+	//IMGUI_CHECKVERSION();
+	//ImGui::CreateContext();
+	//ImGuiIO& io = ImGui::GetIO(); (void)io;
+	////io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	//
+	//ImGui_ImplWin32_Init(handle);
+	//ImGui_ImplDX11_Init(device, deviceContext);
+	//
+	//// Setup style
+	//ImGui::StyleColorsDark();
 }
 
 void ImguiManager::Update()
 {
-	ImGui_ImplDX11_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-
-	//==================================================
-	// Call all the Window rendering calls here
-	//==================================================
-
-
-	void TestWindow();
-
-
-
-	//==================================================
-
-
-	ImGui::Render();
-	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	//ImGui_ImplDX11_NewFrame();
+	//ImGui_ImplWin32_NewFrame();
+	//ImGui::NewFrame();
+	//
+	////==================================================
+	//// Call all the Window rendering calls here
+	////==================================================
+	//
+	//
+	//void TestWindow();
+	//
+	//
+	//
+	////==================================================
+	//
+	//
+	//ImGui::Render();
+	//ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
 
 void ImguiManager::Destroy()
--- BaseEngine/BaseEngine/d3dclass.cpp
+++ BaseEngine/BaseEngine/d3dclass.cpp
@@ -172,7 +172,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 
 	swapChainDesc.Flags = D3D11_CREATE_DEVICE_DEBUG;
 
-	featureLevel = D3D_FEATURE_LEVEL_11_1;	//This is used to set the directx version
+	featureLevel = D3D_FEATURE_LEVEL_11_0;	//This is used to set the directx version
 
 	result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1, D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
 	if (FAILED(result))
