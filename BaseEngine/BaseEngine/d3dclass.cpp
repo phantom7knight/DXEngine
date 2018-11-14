9368fd353e70f01615ac89267a773de697706324
rohit.tolety <rohit.tolety@digipen.edu>
2018-11-13 21:00:26 -0800

    small changes

--- BaseEngine/BaseEngine/d3dclass.cpp
+++ BaseEngine/BaseEngine/d3dclass.cpp
@@ -174,6 +174,7 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 
 	featureLevel = D3D_FEATURE_LEVEL_11_0;	//This is used to set the directx version
 
+	//D3D11_CREATE_DEVICE_DEBUG
 	result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1, D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
 	if (FAILED(result))
 	{
